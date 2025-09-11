654ef7ef295fce4aa0ab3a8a93ad667ea36bc394
dev-ashir768 <techashir167@gmail.com>
2025-09-12 02:20:47 +0500

    remove icon file

--- type/navigation.types.ts
+++ type/navigation.types.ts
@@ -1,9 +1,7 @@
-import { IconProps } from "@/components/ui/foundations/common/icon";
-
 export type NavigationType = {
   title: string;
   description: string;
-  icon: IconProps["name"] | null;
+  icon: string | null;
   dropdown: boolean;
   item: NavigationType[] | null;
   href: string;
